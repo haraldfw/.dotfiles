3f8432122c4955a006633951e59c80c91d5a2c6b
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2025-09-21 14:51:03 +0200

    add emoji font

--- systems/base/fonts.nix
+++ systems/base/fonts.nix
@@ -6,13 +6,16 @@
     fontconfig = {
       defaultFonts = {
         monospace = [ "Fira Code" ];
-        # sansSerif = [  ]
+        sansSerif = [ "Noto Sans" ];
+        serif = [ "Noto Serif" ];
+        emoji = [ "Noto Color Emoji" ];
       };
     };
     packages = with pkgs; [
       fira-code
       fira-code-symbols
       noto-fonts
+      noto-fonts-color-emoji
       font-awesome
     ];
   };
