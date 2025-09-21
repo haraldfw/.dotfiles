fc7a4ff3b87cf7f1f4dc40677059c361c3e5aa9e
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2025-09-21 12:37:03 +0200

    remove some comments

--- modules/core/base.nix
+++ modules/core/base.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { pkgs, inputs, ... }:
 
 {
@@ -13,8 +9,6 @@
     "flakes"
   ];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     home-manager
     git
@@ -29,9 +23,6 @@
     fira-code
   ];
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
   # This value determines the NixOS release from which the default
