d9c9932d429ad24c183d4284e54424305da12f16
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2025-12-09 14:13:30 +0100

    update waybar, not with css

--- home/waybar.nix
+++ home/waybar.nix
@@ -1,171 +1,92 @@
 { ... }:
 {
-  programs.waybar.settings = {
-    height = 30;
-    spacing = 4;
-    modules-left = [
-      "hyprland/workspaces"
-      "hyprland/submap"
-      "custom/media"
-    ];
-    modules-center = [
-      "hyprland/window"
-    ];
-    modules-right = [
-      "mpd"
-      "idle_inhibitor"
-      "pulseaudio"
-      "network"
-      "power-profiles-daemon"
-      "cpu"
-      "memory"
-      "temperature"
-      "backlight"
-      "keyboard-state"
-      "battery"
-      "battery#bat2"
-      "tray"
-      "custom/power"
-      "clock"
-    ];
-    "keyboard-state" = {
-      "numlock" = true;
-      "format" = "{name} {icon}";
-      "format-icons" = {
-        "locked" = "";
-        "unlocked" = "";
-      };
-    };
-    "hyprland/submap" = {
-      "format" = "<span style=\"italic\">{}</span>";
-    };
-    "mpd" = {
-      "format" =
-        "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
-      "format-disconnected" = "Disconnected ";
-      "format-stopped" = "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ";
-      "unknown-tag" = "N/A";
-      "interval" = 5;
-      "consume-icons" = {
-        "on" = " ";
-      };
-      "random-icons" = {
-        "off" = "<span color=\"#f53c3c\"></span> ";
-        "on" = " ";
-      };
-      "repeat-icons" = {
-        "on" = " ";
-      };
-      "single-icons" = {
-        "on" = "1 ";
-      };
-      "state-icons" = {
-        "paused" = "";
-        "playing" = "";
-      };
-      "tooltip-format" = "MPD (connected)";
-      "tooltip-format-disconnected" = "MPD (disconnected)";
-    };
-    "idle_inhibitor" = {
-      "format" = "{icon}";
-      "format-icons" = {
-        "activated" = "";
-        "deactivated" = "";
-      };
-    };
-    "tray" = {
-      # "icon-size"=21;
-      #		 "spacing"=10;
-      #		 # "icons"={
-      #	"blueman"="bluetooth";
-      #		 #	"TelegramDesktop"="$HOME/.local/share/icons/hicolor/16x16/apps/telegram.png"
-      # }
-    };
-    "clock" = {
-      "timezone" = "Europe/Oslo";
-      "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-      "format-alt" = "{:%Y-%m-%d}";
-    };
-    "cpu" = {
-      "format" = "{usage}% ";
-      "tooltip" = false;
-    };
-    "memory" = {
-      "format" = "{}% ";
-    };
-    "temperature" = {
-      # "thermal-zone"=2;
-      #		 # "hwmon-path"="/sys/class/hwmon/hwmon2/temp1_input";
-      #		 "critical-threshold"=80;
-      #		 # "format-critical"="{temperatureC}°C {icon}";
-      #		 "format"="{temperatureC}°C {icon}";
-      #		 "format-icons"=["", "", ""]
-    };
-    "backlight" = {
-      # "device"="acpi_video1";
-      #		 "format"="{percent}% {icon}";
-      #		 "format-icons"=["", "", "", "", "", "", "", "", ""]
-    };
-    "battery" = {
-      "states" = {
-        # "good"=95;
-        #			 "warning"=30;
-        #			 "critical"=15
-      };
-      "format" = "{capacity}% {icon}";
-      "format-full" = "{capacity}% {icon}";
-      "format-charging" = "{capacity}% ";
-      "format-plugged" = "{capacity}% ";
-      "format-alt" = "{time} {icon}";
-      # "format-good"="", # An empty format will hide the module
-      # "format-full"="";
-      #		 "format-icons"=["", "", "", "", ""]
-    };
-    "battery#bat2" = {
-      "bat" = "BAT2";
-    };
-    "power-profiles-daemon" = {
-      "format" = "{icon}";
-      "tooltip-format" = "Power profile={profile}\nDriver={driver}";
-      "tooltip" = true;
-      "format-icons" = {
-        "default" = "";
-        "performance" = "";
-        "balanced" = "";
-        "power-saver" = "";
-      };
-    };
-    "network" = {
-      # "interface"="wlp2*", # (Optional) To force the use of this interface
-      "format-wifi" = "{essid} ({signalStrength}%) ";
-      "format-ethernet" = "{ipaddr}/{cidr} ";
-      "tooltip-format" = "{ifname} via {gwaddr} ";
-      "format-linked" = "{ifname} (No IP) ";
-      "format-disconnected" = "Disconnected ⚠";
-      "format-alt" = "{ifname}={ipaddr}/{cidr}";
-    };
-    "pulseaudio" = {
-      # "scroll-step"=1, # %, can be a float
-      "format" = "{volume}% {icon} {format_source}";
-      "format-bluetooth" = "{volume}% {icon} {format_source}";
-      "format-bluetooth-muted" = " {icon} {format_source}";
-      "format-muted" = " {format_source}";
-      "format-source" = "{volume}% ";
-      "format-source-muted" = "";
-      "format-icons" = {
-        "headphone" = "";
-        "hands-free" = "";
-        "headset" = "";
-        "phone" = "";
-        "portable" = "";
-        "car" = "";
-        "default" = [
-          ""
-          ""
-          ""
+  programs.waybar = {
+    enable = true;
+    style = ''
+      #workspaces button.active {
+        color: #00dd33;
+      }
+    '';
+    settings = {
+      mainBar = {
+        height = 30;
+        spacing = 4;
+        modules-left = [
+          "hyprland/workspaces"
+          "hyprland/submap"
+          "custom/media"
+          "hyprland/window"
         ];
+        modules-center = [
+        ];
+        modules-right = [
+          "idle_inhibitor"
+          "pulseaudio"
+          "network"
+          "cpu"
+          "memory"
+          "tray"
+          "clock"
+        ];
+        "hyprland/workspaces" = {
+          format = "{icon}";
+          on-click = "activate";
+        };
+        "hyprland/submap" = {
+          format = "<span style=\"italic\">{}</span>";
+        };
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "";
+            deactivated = "";
+          };
+        };
+        clock = {
+          timezone = "Europe/Oslo";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          format-alt = "{:%Y-%m-%d}";
+        };
+        cpu = {
+          format = "{usage}% ";
+          tooltip = false;
+        };
+        memory = {
+          format = "{}% ";
+        };
+        network = {
+          # "interface"="wlp2*", # (Optional) To force the use of this interface
+          format-wifi = "{essid} ({signalStrength}%) ";
+          format-ethernet = "{ipaddr}/{cidr} ";
+          tooltip-format = "{ifname} via {gwaddr} ";
+          format-linked = "{ifname} (No IP) ";
+          format-disconnected = "Disconnected ⚠";
+          format-alt = "{ifname}={ipaddr}/{cidr}";
+        };
+        pulseaudio = {
+          # "scroll-step"=1, # %, can be a float
+          format = "{volume}% {icon} {format_source}";
+          format-bluetooth = "{volume}% {icon} {format_source}";
+          format-bluetooth-muted = " {icon} {format_source}";
+          format-muted = " {format_source}";
+          format-source = "{volume}% ";
+          format-source-muted = "";
+          format-icons = {
+            headphone = "";
+            hands-free = "";
+            headset = "";
+            phone = "";
+            portable = "";
+            car = "";
+            default = [
+              ""
+              ""
+              ""
+            ];
+          };
+          on-click = "pavucontrol";
+        };
       };
-      "on-click" = "pavucontrol";
     };
   };
 }
