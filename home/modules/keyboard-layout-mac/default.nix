a6520afa9aae72bdf61e7e2aa234867cf9a63b09
Harald Floor Wilhelmsen <harald.wilhelmsen@sikt.no>
2026-07-31 15:37:54 +0200

    add launch agent which swaps right option and cmd

--- home/modules/keyboard-layout-mac/default.nix
+++ home/modules/keyboard-layout-mac/default.nix
@@ -7,4 +7,17 @@
     $DRY_RUN_CMD cp -RL ${./us-intl-no-dead-keys.bundle} "${config.home.homeDirectory}/Library/Keyboard Layouts/us-intl-no-dead-keys.bundle"
     $DRY_RUN_CMD chmod -R u+w "${config.home.homeDirectory}/Library/Keyboard Layouts/us-intl-no-dead-keys.bundle"
   '';
+
+  launchd.agents.swap-right-option-cmd = {
+    enable = true;
+    config = {
+      ProgramArguments = [
+        "/usr/bin/hidutil"
+        "property"
+        "--set"
+        ''{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E6,"HIDKeyboardModifierMappingDst":0x7000000E7},{"HIDKeyboardModifierMappingSrc":0x7000000E7,"HIDKeyboardModifierMappingDst":0x7000000E6}]}''
+      ];
+      RunAtLoad = true;
+    };
+  };
 }
